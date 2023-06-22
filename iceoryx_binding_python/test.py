33f4acba2af17e4fe59a0bb8a7e22e36b7e4c409
Muhammad Usama Anjum <usama.anjum@collabora.com>
2023-06-22 14:30:50 +0500

    leave and move on

--- iceoryx_binding_python/test.py
+++ iceoryx_binding_python/test.py
@@ -1,10 +1,17 @@
 
-import pyiceoryx
+import iceoryx_binding_python
 
 A = [1.,2.,3.,4.]
 
-B = pyiceoryx.modify(A)
-
+B = iceoryx_binding_python.modify(A)
 print(B)
+
+print("square() working", iceoryx_binding_python.square(10))
+
+str = "ssstringgg"
+iceoryx_binding_python.test(str)
+
+ret = iceoryx_binding_python.initRuntime("app_name")
+print(ret)
 
 
--- iceoryx_binding_python/wrapper.cpp
+++ iceoryx_binding_python/wrapper.cpp
@@ -2,36 +2,84 @@
 #include <pybind11/stl.h>
 #include <vector>
 
+//#include "../iceoryx_hoofs/include/iceoryx_hoofs/cxx/requires.hpp"
+//#include "../iceoryx_posh/include/iceoryx_posh/runtime/posh_runtime.hpp"
+//
+#include "iceoryx_hoofs/cxx/requires.hpp"
+#include "iceoryx_posh/runtime/posh_runtime.hpp"
+
+#include "iox/string.hpp"
+#include <string.h>
+
 // ----------------
 // Regular C++ code
 // ----------------
+//std::vector<double> modify(const std::vector<double>& input)
+//{
+//  std::vector<double> output;
+//
+//  std::transform(
+//    input.begin(),
+//    input.end(),
+//    std::back_inserter(output),
+//    [](double x) -> double { return 2.*x; }
+//  );
+//
+//  return output;
+//}
 
-std::vector<double> modify(const std::vector<double>& input)
-{
-  std::vector<double> output;
+namespace iox {
+	namespace planet {
+	namespace earth {
+	namespace country {
+		int square(int num) {
+			return num * num;
+		}
 
-  std::transform(
-    input.begin(),
-    input.end(),
-    std::back_inserter(output),
-    [](double x) -> double { return 2.*x; }
-  );
+		void printname(const std::string &s) {
+			std::cout << "cpp sys " << s << std::endl;
+		}
+	}
+	}
+	}
 
-  return output;
+//	void *initRuntime_(char *ch) {
+//		constexpr char str[128];
+//		memcpy(str, ch, strlen(ch));
+//		return iox::runtime::PoshRuntime::initRuntime(str);
+//	  }
 }
-//iox::runtime::PoshRuntime::initRuntime(APP_NAME);
+
+namespace iox {
 // ----------------
 // Python interface
 // ----------------
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(pyiceoryx,m)
-{
-  m.doc() = "pybind11 example plugin";
-  m.def("modify", &modify, "Multiply all entries of a list by 2.0");
 
-  m.def("initRuntime", &iox::runtime::PoshRuntime::initRuntime);
+PYBIND11_MODULE(iceoryx_binding_python,m)
+{
+  m.doc() = "Iceoryx pybind11 binding module";
+//  m.def("modify", &modify, "Testing function");
+
+  m.def("square", &planet::earth::country::square);
+  m.def("square", &planet::earth::country::printname);
+
+  m.def("test",
+      [](const std::string &s) {
+	  planet::earth::country::printname(s);
+      }
+  );
+
+//  m.def("initRuntime",
+//	[](const char *s) {
+//	    iox::runtime::PoshRuntime::initRuntime("");
+//  	}
+//  );
+
+
+  //iox::runtime::PoshRuntime::initRuntime(APP_NAME);
+//  m.def("initRuntime", &iox::runtime::PoshRuntime::initRuntime, py::arg("name"));
 
 //  py::class_<iox>(m, iox)
 //      .
@@ -40,5 +88,6 @@ PYBIND11_MODULE(pyiceoryx,m)
 //      .def(py::init<const std::string &>())
 //      .def("setName", &Pet::setName)
 //      .def("getName", &Pet::getName);
+}
 }
 
